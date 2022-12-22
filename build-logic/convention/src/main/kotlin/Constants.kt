23ecd0d75d55579a57fddad909335af2a9554463
Olivér Falvai <ofalvai@gmail.com>
2022-12-22 16:38:43 +0100

    Version 1.0.0

--- build-logic/convention/src/main/kotlin/Constants.kt
+++ build-logic/convention/src/main/kotlin/Constants.kt
@@ -22,6 +22,6 @@ object Constants {
     const val TARGET_SDK = 33
     val JVM_TARGET = JavaVersion.VERSION_1_8
 
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "0.1.0"
+    const val VERSION_CODE = 2
+    const val VERSION_NAME = "1.0.0"
 }
