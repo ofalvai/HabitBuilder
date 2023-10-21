b19f283c75715c5a9c97189bba4ea2b14d968272
Olivér Falvai <ofalvai@gmail.com>
2023-10-21 20:50:57 +0200

    Deprecation fixes

--- build-logic/convention/src/main/kotlin/shared/ComposeMetrics.kt
+++ build-logic/convention/src/main/kotlin/shared/ComposeMetrics.kt
@@ -17,23 +17,22 @@
 package shared
 
 import org.gradle.api.Project
-import java.io.File
 
 fun Project.buildComposeMetricsParameters(): List<String> {
     val metricParameters = mutableListOf<String>()
     val enableMetricsProvider = project.providers.gradleProperty("habittracker.enableComposeCompilerReports")
     val enableMetrics = (enableMetricsProvider.orNull == "true")
     if (enableMetrics) {
-        val metricsFolder = File(project.buildDir, "compose-metrics")
+        val metricsFolder = project.layout.buildDirectory.dir("compose-metrics")
         metricParameters.add("-P")
         metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
+            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.get().asFile.absolutePath
         )
 
-        val reportsFolder = File(project.buildDir, "compose-reports")
+        val reportsFolder = project.layout.buildDirectory.dir("compose-reports")
         metricParameters.add("-P")
         metricParameters.add(
-            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
+            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.get().asFile.absolutePath
         )
     }
 
